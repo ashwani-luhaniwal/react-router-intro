640f17ee88082fbe1cd19333ea203cb8457e24b0
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2017-12-24 10:22:15 -0800

    Adding routes in main along with two new components

--- LocalSportsApp/components/Main.js
+++ LocalSportsApp/components/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Roster from './Roster';
+import Schedule from './Schedule';
 
 export default class Main extends React.Component {
     render() {
         return (
-            <div>Main</div>
+            <main>
+                <Switch>
+                    {/* 
+                        "exact" prop is used to state that the route should only match when pathname 
+                        matches the route's path exactly
+                    */}
+                    <Route exact path="/" component={Home} />
+                    <Route path='/roster' component={Roster} />
+                    <Route path='/schedule' component={Schedule} />
+                </Switch>
+            </main>
         );
     }
 }
---
+++ LocalSportsApp/components/Roster.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Roster extends React.Component {
+    render() {
+        return (
+            <div>Roster</div>
+        );
+    }
+}
