a50d28719aea3f9195b3260a9d10763d5bb1ada6
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2017-12-24 04:13:36 -0800

    Adding Links to Routing

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom'; // Router component from React
+import About from './modules/About';
+import Repos from './modules/Repos';
 import App from './modules/App';
 
 ReactDOM.render(
-    <App />, 
+    (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/about" component={About} />
+                    <Route path="/repos" component={Repos} />
+                </Switch>
+            </div>
+        </Router>
+    ), 
     document.getElementById('app')
 );
--- modules/App.js
+++ modules/App.js
@@ -6,23 +6,14 @@ import Repos from './Repos';
 export default class App extends React.Component {
     render() {
         return (
-            <Router>
-                <div>
-                    <div>
-                        <h1>React Router Introduction</h1>
-                        <ul role="nav">
-                            <li><Link to="/about">About</Link></li>
-                            <li><Link to="/repos">Repos</Link></li>
-                        </ul>
-                        <hr />
-
-                        <Switch>
-                            <Route path="/repos" component={Repos} />
-                            <Route path="/about" component={About} />
-                        </Switch>
-                    </div>
-                </div>
-            </Router>
+            <div>
+                <h1>React Router Introduction</h1>
+                <ul role="nav">
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/repos">Repos</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
         );
     }
 }
