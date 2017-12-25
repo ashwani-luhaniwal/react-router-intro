07871c937add59b68a744cd95828827801e2dfb5
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2017-12-25 09:32:24 -0800

    Fix minor bug fixes

--- ReactRouterWeb/Basic/components/BasicExample.js
+++ ReactRouterWeb/Basic/components/BasicExample.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, Switch, Router } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Topics from './Topics';
--- ReactRouterWeb/Basic/components/Topics.js
+++ ReactRouterWeb/Basic/components/Topics.js
@@ -6,12 +6,14 @@ const Topics = ({match}) => (
     <div>
         <h2>Topics</h2>
         <ul>
+            {/* Create routes based on url match */}
             <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
             <li><Link to={`${match.url}/components`}>Components</Link></li>
-            <li><Link to={`${match.url}/:props-v-state`}>Props v. State</Link></li>
+            <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
         </ul>
 
         <Switch>
+            {/* Redering above mentioned links using Topic component */}
             <Route path={`${match.url}/:topicId`} component={Topic} />
             <Route exact path={match.url} render={() => (
                 <h3>Please select a topic.</h3>
